9da9e9fb24edc411d68bad3876f1b595a6e71ec4
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-08 09:28:35 +0530

    Adding LargestNumber Project

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 10;
+            if(a>b&&a>c)
+            {
+                Console.WriteLine("a is Largest number", +a);
+            }
+            else if(b>c&&b>a)
+            {
+                Console.WriteLine("b is Largest number", +b);
+            }
+            else
+            {
+                Console.WriteLine("c is  larger", +c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
